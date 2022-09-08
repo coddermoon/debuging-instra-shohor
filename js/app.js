c9ddd2b90a1913bd14dae87cd738dab5c04f6a7a
Mahamodul Hasan Moon <coddermoon@gmail.com>
2022-09-09 00:11:06 +0600

    profile pic error done

--- js/app.js
+++ js/app.js
@@ -51,6 +51,9 @@ const switchTab = (id) => {
 };
 
 const createPost = (post) => {
+  console.log(post)
+  const userImg = post.userImage
+
     const image = post.image;
     const div = document.createElement( "article" );
     div.classList.add( "post" );
@@ -62,7 +65,7 @@ const createPost = (post) => {
                     target="_blank"
                     class="post__avatar"
                   >
-                    <img src="${image}" alt="User Picture" />
+                    <img src="${userImg}" alt="User Picture" />
                   </a>
                   <a href="#" class="post__user">phero</a>
                 </div>
